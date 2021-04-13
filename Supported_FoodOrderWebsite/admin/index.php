ec008dbd488390bc53edb58a653a35cbac844f6c
Łukasz Michalik <l.michalik004@gmail.com>
2021-04-13 10:33:55 +0200

    Finish whole project

--- Supported_FoodOrderWebsite/admin/index.php
+++ Supported_FoodOrderWebsite/admin/index.php
@@ -14,24 +14,46 @@
                 ?>
 
                 <div class="col-4 text-center">
-                    <h1>5</h1>
+                    <?php
+                        $sql = "SELECT * FROM ofw_category";
+                        $res = mysqli_query($conn, $sql);
+                        $count = mysqli_num_rows($res);
+                    ?>
+                    <h1><?php echo $count; ?></h1>
                     <br>
-                    categories
+                    Categories
                 </div>
                 <div class="col-4 text-center">
-                    <h1>5</h1>
+                    <?php
+                        $sql2 = "SELECT * FROM ofw_food";
+                        $res2 = mysqli_query($conn, $sql2);
+                        $count2 = mysqli_num_rows($res2);
+                    ?>
+                    <h1><?php echo $count2; ?></h1>
                     <br>
-                    categories
+                    Foods
                 </div>
                 <div class="col-4 text-center">
-                    <h1>5</h1>
+                    <?php
+                        $sql3 = "SELECT * FROM ofw_order";
+                        $res3 = mysqli_query($conn, $sql3);
+                        $count3 = mysqli_num_rows($res3);
+                    ?>
+                    <h1><?php echo $count3; ?></h1>
                     <br>
-                    categories
+                    Total Orders
                 </div>
                 <div class="col-4 text-center">
-                    <h1>5</h1>
+                <?php
+                        $sql4 = "SELECT SUM(total) as total FROM ofw_order WHERE status = 'delivered'";
+                        $res4 = mysqli_query($conn, $sql4);
+                        $row4 = mysqli_fetch_assoc($res4);
+                        $total_revenur = $row4['total'] ?? "0";
+
+                    ?>
+                    <h1>$<?php echo $total_revenur; ?></h1>
                     <br>
-                    categories
+                    Revenue&nbsp;Generated
                 </div>
                 <div class="clearfix"></div>
             </div>
--- Supported_FoodOrderWebsite/supp_order_food_web.sql
+++ Supported_FoodOrderWebsite/supp_order_food_web.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 29 Mar 2021, 22:21
+-- Czas generowania: 13 Kwi 2021, 10:32
 -- Wersja serwera: 10.4.14-MariaDB
 -- Wersja PHP: 7.4.11
 
@@ -34,6 +34,18 @@ CREATE TABLE `ofw_admin` (
   `password` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Zrzut danych tabeli `ofw_admin`
+--
+
+INSERT INTO `ofw_admin` (`id`, `full_name`, `username`, `password`) VALUES
+(32, 'Karol', 'Baczek', '202cb962ac59075b964b07152d234b70'),
+(33, 'Mateusz', 'Smak', '202cb962ac59075b964b07152d234b70'),
+(34, 'Bary', 'Gagarin', '202cb962ac59075b964b07152d234b70'),
+(35, 'Witold', 'Sroda', '202cb962ac59075b964b07152d234b70'),
+(36, 'Ala', 'Marik', '202cb962ac59075b964b07152d234b70'),
+(37, 'a', 'a', '0cc175b9c0f1b6a831c399e269772661');
+
 -- --------------------------------------------------------
 
 --
@@ -47,6 +59,15 @@ CREATE TABLE `ofw_category` (
   `featured` varchar(10) DEFAULT NULL,
   `active` varchar(10) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Zrzut danych tabeli `ofw_category`
+--
+
+INSERT INTO `ofw_category` (`id`, `title`, `image_name`, `featured`, `active`) VALUES
+(59, 'Pizza', 'Food_Category_46.jpg', 'yes', 'yes'),
+(60, 'Burger', 'Food_Category_250.jpg', 'yes', 'yes'),
+(61, 'Momo', 'Food_Category_326.jpg', 'yes', 'yes');
 
 -- --------------------------------------------------------
 
@@ -65,6 +86,18 @@ CREATE TABLE `ofw_food` (
   `active` varchar(10) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Zrzut danych tabeli `ofw_food`
+--
+
+INSERT INTO `ofw_food` (`id`, `title`, `description`, `price`, `image_name`, `category_id`, `featured`, `active`) VALUES
+(18, 'Capricossa', 'Delicious Pizza', '25.00', 'Food_Name_3607.jpg', 59, 'yes', 'yes'),
+(19, 'Lamp Dumplings', 'Wonderful Dumplings', '19.00', 'Food_Name_9046.jpg', 61, 'yes', 'yes'),
+(20, 'Banger King', 'Fast Food', '15.00', 'Food_Name_4764.jpg', 60, 'yes', 'yes'),
+(21, 'Pizza Italiana', 'Pizza with chesse', '23.00', 'Food_Name_7822.jpg', 59, 'yes', 'yes'),
+(22, 'Burger Chisi', 'Burgerro', '12.00', 'Food_Name_6285.jpg', 60, 'yes', 'yes'),
+(24, 'Piergos Banditos', 'Delicious Piergi Polskie', '35.00', 'Food_Name_3943.jpg', 61, 'yes', 'yes');
+
 -- --------------------------------------------------------
 
 --
@@ -75,15 +108,22 @@ CREATE TABLE `ofw_order` (
   `id` int(11) NOT NULL,
   `food` varchar(150) DEFAULT NULL,
   `price` varchar(255) DEFAULT NULL,
-  `qty` decimal(10,2) DEFAULT NULL,
-  `total` varchar(255) DEFAULT NULL,
-  `order_date` int(11) DEFAULT NULL,
-  `status` varchar(10) DEFAULT NULL,
+  `qty` int(11) DEFAULT NULL,
+  `total` decimal(5,2) DEFAULT NULL,
+  `order_date` varchar(30) DEFAULT NULL,
+  `status` varchar(30) DEFAULT NULL,
   `customer_name` varchar(10) DEFAULT NULL,
   `customer_contact` varchar(30) DEFAULT NULL,
   `customer_email` varchar(30) DEFAULT NULL,
   `customer_address` varchar(30) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Zrzut danych tabeli `ofw_order`
+--
+
+INSERT INTO `ofw_order` (`id`, `food`, `price`, `qty`, `total`, `order_date`, `status`, `customer_name`, `customer_contact`, `customer_email`, `customer_address`) VALUES
+(14, 'Pizza Italiana', '23.00', 2, '46.00', '2021-04-12 08:19:00pm', 'delivered', 'adsaasd', '123', 'ads@eweq', 'sdaasdasd');
 
 --
 -- Indeksy dla zrzutów tabel
@@ -105,7 +145,8 @@ ALTER TABLE `ofw_category`
 -- Indeksy dla tabeli `ofw_food`
 --
 ALTER TABLE `ofw_food`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `category_id` (`category_id`);
 
 --
 -- Indeksy dla tabeli `ofw_order`
@@ -121,25 +162,35 @@ ALTER TABLE `ofw_order`
 -- AUTO_INCREMENT dla tabeli `ofw_admin`
 --
 ALTER TABLE `ofw_admin`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
 
 --
 -- AUTO_INCREMENT dla tabeli `ofw_category`
 --
 ALTER TABLE `ofw_category`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=62;
 
 --
 -- AUTO_INCREMENT dla tabeli `ofw_food`
 --
 ALTER TABLE `ofw_food`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT dla tabeli `ofw_order`
 --
 ALTER TABLE `ofw_order`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- Ograniczenia dla zrzutów tabel
+--
+
+--
+-- Ograniczenia dla tabeli `ofw_food`
+--
+ALTER TABLE `ofw_food`
+  ADD CONSTRAINT `ofw_food_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `ofw_category` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
